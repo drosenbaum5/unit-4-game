a8f0de002b95e22f6737e6f6929ee00f62ecbf54
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-17 23:39:59 -0400

    Added additional containers for cards. Positioned and styled remaining assets, including cards for last two infinity stones.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,3 +2,8 @@ var firstCrystal = [];
 var secondCrystal = [];
 var thirdCrystal = [];
 var fourthCrystal = [];
+
+
+Math.random
+
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
