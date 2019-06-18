3737c551fee8981c9b6ea025da29293678970bec
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-18 12:52:14 -0400

    Added styling for player count and thanos count. Linked to element id's using jQuery. Successfully displayed thanos and user score.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,16 @@ var soulStone;
 var timeStone;
 var thanosNumber;
 var playerNumber = 0;
-var wins;
-var losses;
-
-
-
-
+var wins = 0;
+var losses = 0;
 
 
 function gameStart () {
 //generate a random number for thanos
 thanosNumber = [Math.floor(Math.random() * 121)+ 19];
-// $("number-thanos").html("<h3>" + thanosNumber + "</h3>");
+playerNumber = 0;
+$("#thanos-num").html(thanosNumber);
+$("#player-num").html(playerNumber);
 
 
 //generate random numbers for the infinity stone cards
@@ -23,39 +21,92 @@ powerStone = [Math.floor(Math.random() * 13)];
 realityStone = [Math.floor(Math.random() * 13) + 1];
 soulStone = [Math.floor(Math.random() * 13) + 1];
 timeStone = [Math.floor(Math.random() * 13) + 1];
-
-
 }
 
 //call function gameStart to set all variable values
 gameStart ();
 
 
-//Add to playerNumber by clicking on powerStone
 
+
+//Add to playerNumber by clicking on powerStone
 function stoneValues () {
 $('#powerstone').on("click", function(){
 playerNumber += parseInt(powerStone); 
+$("#player-num").html(playerNumber);
 console.log(playerNumber);
+
+if(playerNumber === thanosNumber) {
+
+    wins++
+    
+    gameStart();
+}
+
+ else if (playerNumber > thanosNumber) {
+
+    losses++
+    gameStart();
+}
+
 
 })
 
 //Add to playerNumber by clicking on realityStone
 $('#realitystone').on("click", function(){
     playerNumber += parseInt(realityStone); 
+    $("#player-num").html(playerNumber);
     console.log(playerNumber);
+
+    if(playerNumber === thanosNumber) {
+
+        wins++
+        gameStart();
+    }
+    
+     else if (playerNumber > thanosNumber) {
+    
+        losses++
+        gameStart();
+    }
 })
     
 //Add to playerNumber by clicking on soulStone
 $('#soulstone').on("click", function(){
     playerNumber += parseInt(soulStone); 
+    $("#player-num").html(playerNumber);
     console.log(playerNumber);
+
+    if(playerNumber === thanosNumber) {
+
+        wins++
+        gameStart();
+    }
+    
+     else if (playerNumber > thanosNumber) {
+    
+        losses++
+        gameStart();
+    }
 })
 
 //Add to playerNumber by clicking on timeStone
 $('#timestone').on("click", function(){
     playerNumber += parseInt(timeStone); 
+    $("#player-num").html(playerNumber);
     console.log(playerNumber);
+
+    if(playerNumber === thanosNumber) {
+
+        wins++
+        gameStart();
+    }
+    
+     else if (playerNumber > thanosNumber) {
+    
+        losses++
+        gameStart();
+    }
 })
 
 }
@@ -70,15 +121,3 @@ stoneValues();
 
 
 
-if(playerNumber === thanosNumber) {
-
-    wins++
-    gameStart();
-}
-
-else if (playerNumber > thanosNumber) {
-
-    losses++
-    
-
-}
