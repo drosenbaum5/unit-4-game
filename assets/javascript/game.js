c1f6fc63dbf96355442a783fcb7bde05a67f1f37
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-18 11:09:51 -0400

    Added parseInt to stone click events. Wrapped stone click events in stoneValues() function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,45 +15,58 @@ var losses;
 function gameStart () {
 //generate a random number for thanos
 thanosNumber = [Math.floor(Math.random() * 121)+ 19];
+// $("number-thanos").html("<h3>" + thanosNumber + "</h3>");
+
 
 //generate random numbers for the infinity stone cards
-powerStone = [Math.floor(Math.random() * 13)+ 1];
-realityStone = [Math.floor(Math.random() * 13)+ 1];
-soulStone = [Math.floor(Math.random() * 13)+ 1];
-timeStone = [Math.floor(Math.random() * 13)+ 1];
+powerStone = [Math.floor(Math.random() * 13)];
+realityStone = [Math.floor(Math.random() * 13) + 1];
+soulStone = [Math.floor(Math.random() * 13) + 1];
+timeStone = [Math.floor(Math.random() * 13) + 1];
+
 
 }
 
+//call function gameStart to set all variable values
+gameStart ();
+
 
 //Add to playerNumber by clicking on powerStone
+
+function stoneValues () {
 $('#powerstone').on("click", function(){
-playerNumber += powerStone; 
+playerNumber += parseInt(powerStone); 
 console.log(playerNumber);
+
 })
 
 //Add to playerNumber by clicking on realityStone
 $('#realitystone').on("click", function(){
-    playerNumber += realityStone; 
+    playerNumber += parseInt(realityStone); 
     console.log(playerNumber);
 })
     
 //Add to playerNumber by clicking on soulStone
 $('#soulstone').on("click", function(){
-    playerNumber += soulStone; 
+    playerNumber += parseInt(soulStone); 
     console.log(playerNumber);
 })
 
 //Add to playerNumber by clicking on timeStone
 $('#timestone').on("click", function(){
-    playerNumber += timeStone; 
+    playerNumber += parseInt(timeStone); 
     console.log(playerNumber);
 })
-    
 
-if(playerNumber !== thanosNumer) {
-
-    
 }
+
+stoneValues();
+    
+
+// if(playerNumber !== thanosNumer) {
+
+    
+// }
 
 
 
